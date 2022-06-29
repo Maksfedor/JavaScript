8dffa2f66775288aed1d01cd6845ed98fe8e79c3
maksim <maksim1991fedovovskiy@gmail.com>
2022-06-29 20:56:52 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// 1 Переменные
+// variable
+
+// const firstName = 'Maksim'
+// const age = 32 // number
+// const isProgrammer = true // boolean
+
+// const _privat = 'privat'
+// const $ = 'some value'
+
+// const withNum5 = '5'
+
+// 2 Мутирование
+
+// alert('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+
+// const lastName = prompt('Введите фамилию')
+// alert(firstName + ' ' + lastName)
+
+// 3 Операторы
+// let currentYear = 2022
+// const birthYear = 1991
+
+// const age = currentYear - birthYear
+// const a = 10
+// const b = 5
+
+// let c = 32
+
+// c = c + a
+// c += a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(currentYear++)
+// console.log(currentYear)
+// console.log(c)
+
+//  4 Типы данных
+
+// const isProgrammer = true
+// const name = 'Maksim'
+// const age = 26
+// let x
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x)
+
+// 5 Приоритет операторов
+
+// const fullAge = 26
+// const birthYear = 1993
+// const currentYear = 2020
+
+// const isFullAge = currentYear - birthYear >= fullAge // 26 >= 27 => true
+// console.log(isFullAge)
+
+// 6 Условные операторы
+
+// const coursStatus = 'fail' // ready, fail, pending
+
+// if (coursStatus === 'ready') {
+//   console.log('Курс уже готов и его можно проходить')
+// } else if (coursStatus === 'pending') {
+//   console.log('Курс находится в процессе разработки')
+// } else {
+//   console.log('Курс не получился')
+// }
+
+const isReady = true
+
+// if (isReady) {
+//   console.log('Все готово')
+// } else {
+//   console.log('Все не готово')
+// }
+
+// Тернарное выражение
+isReady ? console.log('все готово') : console.log('Все не готово')
+
+// const num1 = 42 //number
+// const num2 = '42' // string
+// console.log(num1 === num2)
